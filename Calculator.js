3bf0183f19b60a5278b9f4d9a1979c1682586197
Winnie Poon <winnie.y.poon@gmail.com>
2025-07-26 00:05:53 +1000

    Add function for handling input, reset, backspace, evaluate

--- Calculator.js
+++ Calculator.js
@@ -11,6 +11,7 @@ const multiply = function (a, b) {
 };
 
 const divide = function (a, b) {
+
     return a / b;
 };
 
@@ -38,7 +39,8 @@ let operator;
 let toggleNum = true; //true is when an operator has not been pressed
 const operationBtn = document.querySelectorAll(".operator");
 const numberBtns = document.querySelectorAll(".number");
-const displayOperation = document.querySelector("#display-operation")
+const displayOperation = document.querySelector("#display-operation");
+let shouldReset = false;
 
 function displayOnScreen() {
     let displayOutput;
@@ -55,46 +57,57 @@ function displayOnScreen() {
     displayOperation.textContent = displayOutput;
 }
 
+//this handleInput function creates the rules of what number inputs are allowed
+function handleInput(currentValue, input) {
+    //if first input
+    if (!currentValue) {
+        return input;
+    }
+
+    const isInputDecimal = input === ".";
+    const isInputNumber = !isNaN(input);
+    const alreadyHasDecimal = currentValue.includes(".");
+
+    //Prevent multiple decimals
+    if (alreadyHasDecimal && isInputDecimal) {
+        return currentValue;
+    }
+
+    //Adding zero at front if input starts with decimal
+    if (currentValue.startsWith(".")) {
+        currentValue = "0" + currentValue;
+    }
+
+    //Replace leading zero if input is a number
+    if (currentValue === "0" && isInputNumber) {
+        return input;
+    }
+
+    return currentValue + input;
+}
+
 function setupNumberButtons() {
     numberBtns.forEach(button => {
         button.addEventListener("click", function () {
+            let input = this.getAttribute("data-message");
+
+            if (shouldReset) {
+                num1 = "0";
+                num2 = undefined;
+                operator = undefined;
+                displayResult.textContent = "";
+                shouldReset = false;
+            }
+
             if (toggleNum) {
-                if (num1 === undefined) {
-                    if (this.getAttribute("data-message") !== "0" && this.getAttribute("data-message") !== ".") {
-                        num1 = this.getAttribute("data-message");
-                    }
-                } else {
-                    if (!Number.isInteger(Number(num1))) {
-                        if (this.getAttribute("data-message") !== ".") {
-                            num1 += this.getAttribute("data-message")
-                        }
-                    } else {
-                        num1 += this.getAttribute("data-message");
-                    }
-                }
-
-
+                //making num1 if there isn't an operator
+                num1 = handleInput(num1, input);
             } else {
-                if (num2 === undefined) {
-                    if (this.getAttribute("data-message") !== "0" && this.getAttribute("data-message") !== ".") {
-                        num2 = this.getAttribute("data-message");
-                    }
-                } else {
-                    if (!Number.isInteger(Number(num2))) {
-                        if (this.getAttribute("data-message") !== ".") {
-                            num2 += this.getAttribute("data-message")
-                        }
-                    } else {
-                        num2 += this.getAttribute("data-message");
-                    }
-                }
+                //making num2 if there is an operator
+                num2 = handleInput(num2, input);
             }
 
             displayOnScreen();
-            console.log(num1);
-            console.log(num2)
-            console.log(operator);
-            console.log(toggleNum)
         });
     });
 }
@@ -106,17 +119,13 @@ function setupOperation() {
                 evaluateResult();
                 operator = this.getAttribute("data-message");
                 displayOnScreen();
-                toggleNum = false;
+                toggleNum = false; //toggle will switch between num1 and num2
             } else {
                 operator = this.getAttribute("data-message");
                 //"operator" is a string
                 displayOnScreen();
                 toggleNum = false;
             }
-            console.log(num1);
-            console.log(num2);
-            console.log(operator);
-            console.log(toggleNum);
         })
     })
 }
@@ -124,36 +133,40 @@ function setupOperation() {
 setupNumberButtons()
 setupOperation()
 
-const equalsBtn = document.getElementById("equals-button");
+
 const displayResult = document.querySelector("#display-result");
 
 function evaluateResult() {
     if (num1 && num2 && operator) {
-        let result = operate(operator, Number(num1), Number(num2));
-        displayResult.textContent = result;
-        toggleNum = true;
-        num1 = result;
-        num2 = undefined;
-        operator = undefined;
-        console.log(num1);
-        console.log(num2)
-        console.log(operator);
-        console.log(toggleNum)
+        if (num2 === "0" && operator === "/") {
+            alert("Oops! You cannot divide by 0")
+        } else {
+            let result = operate(operator, Number(num1), Number(num2));
+            displayResult.textContent = result;
+            toggleNum = true;
+            num1 = String(result);
+            num2 = undefined;
+            operator = undefined;
+        }
+
+        shouldReset = true;
     }
 }
 
+const equalsBtn = document.getElementById("equals-button");
 equalsBtn.addEventListener("click", evaluateResult);
 
 
 const resetBtn = document.getElementById("reset-button");
+
 resetBtn.addEventListener("click", function () {
     let confirmed = confirm("Do you really want to clear all data?");
     if (confirmed) {
-        num1 = undefined;
+        num1 = "0";
         num2 = undefined;
         operator = undefined;
         displayOperation.textContent = "0";
-        displayResult.textContent = "0";
+        displayResult.textContent = "";
     } else {
         alert("Reset canceled. Phew, that was lucky we checked ;)");
     }
@@ -168,7 +181,9 @@ backspaceBtn.addEventListener("click", function () {
         num2 = num2.substring(0, num2.length - 1);
         displayOnScreen();
     } else {
+        console.log(num1);
         num1 = num1.substring(0, num1.length - 1);
         displayOnScreen();
     }
 })
+
